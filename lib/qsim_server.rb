2569cca09da31336c39d6eef0eb042d8c9bf1606
Federico Aloi <federico.aloi@gmail.com>
2016-03-02 19:14:52 -0300

    Incrementados limites para que corra QSim

--- lib/qsim_server.rb
+++ lib/qsim_server.rb
@@ -2,7 +2,8 @@ require 'mumukit'
 
 Mumukit.configure do |config|
   config.runner_name = 'qsim-server'
-
+  config.command_size_limit = 4096
+  config.command_time_limit = 30
 end
 
 require_relative './test_hook'
