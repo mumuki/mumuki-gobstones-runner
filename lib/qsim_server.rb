7dbd8d19ed39487b54780faec6cad1ca10a50e4f
Federico Aloi <federico.aloi@gmail.com>
2016-08-10 14:09:34 -0300

    Moved i18n initialization to qsim_server

--- lib/qsim_server.rb
+++ lib/qsim_server.rb
@@ -1,12 +1,14 @@
+require 'i18n'
 require 'mumukit'
+require 'erb'
+
+I18n.load_path += Dir[File.join('.', 'locales', '*.yml')]
 
 Mumukit.runner_name = 'qsim'
 Mumukit.configure do |config|
   config.docker_image = 'mumuki/mumuki-qsim-worker'
   config.content_type = 'html'
 end
-
-require 'erb'
 
 require_relative './metatest'
 require_relative './test_hook'
