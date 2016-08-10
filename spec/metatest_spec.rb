4b6b259d205751628b654232863487f8b944c78b
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-08-10 01:49:24 -0300

    Implementing test framework for qsim
    It is kinda generic, but supports only record postconditions.

--- spec/metatest_spec.rb
+++ spec/metatest_spec.rb
@@ -18,14 +18,14 @@ module Mumukit::Metatest
 
   class Checker
     def check(value, example)
-      example[:postconditions].map { |key, arg| partial key, value, arg, example }
-    end
-
-    def partial(key, value, arg, example)
-      send "check_#{key}", value, arg
+      example[:postconditions].each { |key, arg| check_assertion key, value, arg, example }
       [example[:name], :passed, nil]
     rescue => e
       [example[:name], :failed, e.message]
+    end
+
+    def check_assertion(key, value, arg, example)
+      send "check_#{key}", value, arg
     end
 
     def fail(message)
@@ -40,7 +40,7 @@ module Mumukit::Metatest
     end
 
     def test(compilation, examples)
-      examples.map { |it| example(compilation, it) }
+      [examples.map { |it| example(compilation, it) }]
     rescue Aborted => e
       [e.message, :aborted]
     rescue Errored => e
@@ -63,60 +63,84 @@ class TextChecker < Mumukit::Metatest::Checker
   end
 end
 
+class QsimChecker < Mumukit::Metatest::Checker
+  def check_compare(result, records)
+    records.each do |record, value|
+      fail "#{record} should be #{value}, but was #{result[:records][record]}" unless result[:records][record] == value
+    end
+  end
+end
 
 describe 'metatest' do
   let(:result) { framework.test compilation, examples }
 
   describe 'qsim' do
     let(:framework) do
-      Mumukit::Metatest::Framework.new checker: ,
-                                       operations: {
-                                           run: lambda { |it| run_qsim_code_here it }
-                                       },
-                                       assertions: {
-                                           compare: lambda { |it| it[:result][:records][it[:record]] === it[:value] }
-                                       }
+      Mumukit::Metatest::Framework.new checker: QsimChecker.new,
+                                       runner: Mumukit::Metatest::IdentityRunner.new
+    end
+    let(:compilation) do
+      {
+          special_records: {
+              PC: "0005",
+              SP: "FFEF",
+              IR: "28E5 "
+          },
+          flags: {
+              N: 0,
+              Z: 0,
+              V: 0,
+              C: 0
+          },
+          records: {
+              R0: "0000",
+              R1: "0000",
+              R2: "0000",
+              R3: "0000",
+              R4: "0003",
+              R5: "0004",
+              R6: "0000",
+              R7: "0000"
+          }
+      }
     end
     context 'simple qsim test' do
-      let(:compilation) { 'MOV R3, 0x0003' }
       let(:examples) {
         [{
-             name: 'R3 is 0x0003',
+             name: 'R3 is 0003',
              preconditions: [],
              operation: :run,
-             postconditions: {compare: {R3: '0x0003'}}
+             postconditions: {compare: {R3: '0003'}}
          }]
       }
-      it { expect(result).to eq [['R3 is 0x0003', :failed, 'R3 should be 0x0003, but was 0x0000']] }
+      it { expect(result).to eq [[['R3 is 0003', :failed, 'R3 should be 0003, but was 0000']]] }
     end
 
     context 'simple qsim test with implicit preconditions and operation' do
-      let(:compilation) { 'MOV R3, 0x0003' }
       let(:examples) {
         [{
-             name: 'R3 is 0x0003',
-             postconditions: {compare: {R3: '0x0003'}}
+             name: 'R3 is 0003',
+             postconditions: {compare: {R3: '0003'}}
          }]
       }
-      it { expect(result).to eq [['R3 is 0x0003', :failed, 'R3 should be 0x0003, but was 0x0000']] }
+      it { expect(result).to eq [[['R3 is 0003', :failed, 'R3 should be 0003, but was 0000']]] }
     end
 
     context 'simple qsim test with explicit preconditions' do
-      let(:compilation) { 'MOV R3, 0x0003' }
       let(:examples) {
         [{
-             name: 'R3 is 0x0003',
-             preconditions: {R3: '0x0001', R4: '0x0003'},
-             postconditions: {compare: {R3: '0x0003'}}
+             name: 'R3 is 0003',
+             preconditions: {R3: '0001', R4: '0003'},
+             postconditions: {compare: {R3: '0003'}}
          },
          {
-             name: 'R4 is 0x0003',
-             preconditions: {R3: '0x0001', R4: '0x0003'},
-             postconditions: {compare: {R4: '0x0003'}}
+             name: 'R4 is 0003',
+             preconditions: {R3: '0001', R4: '0003'},
+             postconditions: {compare: {R4: '0003'}}
          }]
       }
-      it { expect(result).to eq [['R3 is 0x0003', :failed, 'R3 should be 0x0003, but was 0x0000'],
-                                 ['R4 is 0x0003', :passed, nil]] }
+      it { expect(result).to eq [[['R3 is 0003', :failed, 'R3 should be 0003, but was 0000'],
+                                  ['R4 is 0003', :passed, nil]]] }
     end
   end
 
