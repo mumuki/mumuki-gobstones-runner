deaf5f3df99bcf571a44805d5775126c8e6f56d7
Federico Aloi <federico.aloi@gmail.com>
2016-08-10 23:21:58 -0300

    Specified test framework

--- lib/metadata_hook.rb
+++ lib/metadata_hook.rb
@@ -1,13 +1,18 @@
 class QsimMetadataHook < Mumukit::Hook
   def metadata
-    {language: {
+    {
+      language: {
         name: 'qsim',
         icon: {type: 'devicon', name: 'qsim'},
         version: 'v0.2.2',
         extension: 'qsim',
         ace_mode: 'assembly_x86',
         graphic: true
+      },
+      test_framework: {
+        name: 'metatest',
+        test_extension: 'yml'
+      }
     }
-}
   end
 end
