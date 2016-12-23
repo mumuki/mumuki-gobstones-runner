98c948de9c85c8552e52bf3944dbc0d3836c74bb
Rodrigo Alfonso <rodri042@gmail.com>
2016-12-23 03:15:01 +0000

    Corrections of PR

--- lib/html_renderer.rb
+++ lib/html_renderer.rb
@@ -5,69 +5,64 @@ module Gobstones
     end
 
     def render_success(result)
-      bind_result(
-        boards: prepare_boards([:initial, :final], result),
-        reason: prepare_reason(result[:reason])
-      )
+      bind_result boards: prepare_boards([:initial, :final], result),
+                  reason: prepare_reason(result[:reason])
     end
 
     def render_error_check_final_board_failed_different_boards(result)
-      bind_result(
-        error: :check_final_board_failed_different_boards,
-        boards: prepare_boards([:initial, :expected, :actual], result)
-      )
+      bind_result error: :check_final_board_failed_different_boards,
+                  boards: prepare_boards([:initial, :expected, :actual], result)
     end
 
     def render_error_check_final_board_failed_unexpected_boom(result)
-      bind_result(
-        error: :check_final_board_failed_unexpected_boom,
-        boards: prepare_boards([:initial, :expected, :actual], result),
-        reason: prepare_reason(result[:reason])
-      )
+      bind_result error: :check_final_board_failed_unexpected_boom,
+                  boards: prepare_boards([:initial, :expected, :actual], result),
+                  reason: prepare_reason(result[:reason])
     end
 
     def render_error_check_error_failed_expected_boom(result)
-      bind_result(
-        error: :check_error_failed_expected_boom,
-        boards: prepare_boards([:initial, :expected, :final], result)
-      )
+      bind_result error: :check_error_failed_expected_boom,
+                  boards: prepare_boards([:initial, :expected, :final], result)
     end
 
     def render_error_check_error_failed_another_reason(result)
-      bind_result(
-        error: :check_error_failed_another_reason,
-        error_parameter: I18n.t("code_#{result[:expected_code]}"),
-        reason: prepare_reason(result[:reason])
-      )
+      bind_result error: :check_error_failed_another_reason,
+                  error_parameter: I18n.t("code_#{result[:expected_code]}"),
+                  reason: prepare_reason(result[:reason])
     end
 
     private
 
     def prepare_reason(reason)
-      return if not reason
-      reason[:message]
+      reason[:message] if reason
     end
 
     def prepare_boards(names, result)
-      names.reject { |it|
-        it == :initial and not @options[:show_initial_board]
-      }.map { |it|
+      visible_names(names).map do |it|
         struct title: "#{it}_board".to_sym,
-               board: (
-                  if result[it] == :boom.to_s
-                    adapt_to_view(result[:initial], true)
-                  else
-                    adapt_to_view(result[it])
-                  end
-               )
-      }
+               board: visible_board(result, it)
+      end
+    end
+
+    def visible_names(names)
+      @options[:show_initial_board] ? names : names - [:initial]
+    end
+
+    def visible_board(result, name)
+      board = result[name]
+
+      if board == :boom.to_s
+        adapt_to_view(result[:initial], true)
+      else
+        adapt_to_view(board)
+      end
     end
 
     def adapt_to_view(board, boom = false)
       return {
-        size: JSON.generate({ x: board[:sizeX], y: board[:sizeY]}),
-        header: JSON.generate({ x: board[:x], y: board[:y] }),
-        table: JSON.generate(board[:table][:json]),
+        size: { x: board[:sizeX], y: board[:sizeY] }.to_json,
+        header: { x: board[:x], y: board[:y] }.to_json,
+        table: board[:table][:json].to_json,
         boom: boom
       }
     end
