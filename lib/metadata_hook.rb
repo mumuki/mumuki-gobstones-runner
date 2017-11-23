429c666b5f13cb508b0c2ecc60a0cc86466211a6
Julian Berbel Alt <julianberbel@hotmail.com>
2017-11-23 16:36:15 -0300

    Add test template to metadata

--- lib/metadata_hook.rb
+++ lib/metadata_hook.rb
@@ -11,7 +11,25 @@ class GobstonesMetadataHook < Mumukit::Hook
       },
       test_framework: {
         name: 'metatest',
-        test_extension: 'yml'
+        test_extension: 'yml',
+        template: <<gobstones
+check_head_position: true
+
+examples:
+ - title: '{{ test_template_sample_description }}'
+   initial_board: |
+     GBB/1.0
+     size x y
+     cell x y Negro 1
+     cell x y Rojo 1
+     head x y
+   final_board: |
+     GBB/1.0
+     size x y
+     cell x y Negro 1
+     cell x y Rojo 0
+     head x y
+gobstones
       }
     }
   end
