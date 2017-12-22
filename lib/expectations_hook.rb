0c0b0cc6c4de14925dcbbe06ff4a36d565019e29
Rodrigo Alfonso <rodri042@gmail.com>
2017-12-22 07:17:36 +0000

    Rewriting expectations hook

--- lib/expectations_hook.rb
+++ lib/expectations_hook.rb
@@ -1,9 +1,35 @@
-# class GobstonesExpectationsHook < Mumukit::Templates::MulangExpectationsHook
-#   include_smells true
-#
-#   def language
-#     'Mulang'
-#   end
-# end
+class GobstonesExpectationsHook < Mumukit::Templates::MulangExpectationsHook
+  include Mumukit::WithTempfile
 
-# // TODO: Fix
+  include_smells true
+
+  def language
+    'Mulang'
+  end
+
+  def ast_command_line(filename)
+    "gobstones-cli --mulang_ast #{filename}"
+  end
+
+  def compile_content(source)
+    ast, status = isolated_run! write_tempfile!(source)
+
+    if status != :passed
+      raise Exception.new("Unable to get Mulang AST - Command failed with status: #{status}")
+    end
+
+    ast
+  rescue => e
+    raise Mumukit::CompilationError, e
+  end
+
+  private
+
+  def isolated_run!(file)
+    env = Mumukit::IsolatedEnvironment.new
+    env.configure!(file) { |filename| ast_command_line(filename) }
+    env.run!
+  ensure
+    env.destroy!
+  end
+end
