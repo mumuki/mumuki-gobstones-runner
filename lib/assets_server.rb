86e57e010c414e919c1ca1c0cf142e8a3b54df12
Rodrigo Alfonso <rodri042@gmail.com>
2017-12-29 05:26:49 +0000

    Fixing MIME types

--- lib/assets_server.rb
+++ lib/assets_server.rb
@@ -1,13 +1,13 @@
 require 'sinatra/base'
 
 class Mumukit::Server::App < Sinatra::Base
-  def self.get_asset(route, path)
+  def self.get_asset(route, path, type)
     get "/assets/#{route}" do
-      send_file File.expand_path("bower_components/#{path}")
+      send_file File.expand_path("bower_components/#{path}"), { type: type }
     end
   end
 
-  get_asset 'webcomponents.js', 'webcomponentsjs/webcomponents.min.js'
-  get_asset 'polymer.html', 'gs-board/dist/polymer.html'
-  get_asset 'gs-board.html', 'gs-board/dist/gs-board.html'
+  get_asset 'webcomponents.js', 'webcomponentsjs/webcomponents.min.js', 'application/javascript'
+  get_asset 'polymer.html', 'gs-board/dist/polymer.html', 'text/html'
+  get_asset 'gs-board.html', 'gs-board/dist/gs-board.html', 'text/html'
 end
