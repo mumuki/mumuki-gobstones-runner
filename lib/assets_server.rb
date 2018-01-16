fa047c383db6a5a21ed8d9af9b9ec4010786ab55
Rodrigo Alfonso <rodri042@gmail.com>
2018-01-16 07:29:45 +0000

    Playing serving a custom editor

--- lib/assets_server.rb
+++ lib/assets_server.rb
@@ -14,9 +14,17 @@ class Mumukit::Server::App < Sinatra::Base
     end
   end
 
-  get_asset 'webcomponents.js',   'javascripts/vendor/webcomponents.min.js',   'application/javascript'
-  get_asset 'polymer.html',       'htmls/vendor/polymer.html',                 'text/html'
-  get_asset 'polymer-mini.html',  'htmls/vendor/polymer-mini.html',            'text/html'
-  get_asset 'polymer-micro.html', 'htmls/vendor/polymer-micro.html',           'text/html'
-  get_asset 'gs-board.html',      'htmls/gs-board.html', 'text/html'
+  def self.get_local_asset(route, path, type)
+    get "/assets/#{route}" do
+      send_file File.expand_path("ATEMPORAL/#{path}"), { type: type } # TODO: Mover a una gema
+    end
+  end
+
+  get_asset 'webcomponents.js',        'javascripts/vendor/webcomponents.min.js',   'application/javascript'
+  get_asset 'polymer.html',            'htmls/vendor/polymer.html',                 'text/html'
+  get_asset 'polymer-mini.html',       'htmls/vendor/polymer-mini.html',            'text/html'
+  get_asset 'polymer-micro.html',      'htmls/vendor/polymer-micro.html',           'text/html'
+  get_asset 'gs-board.html',           'htmls/gs-board.html',                       'text/html'
+  get_local_asset 'gs-element-blockly.html', 'gs-element-blockly.html',                   'text/html'
+  get_local_asset 'gs-element-blockly.css',  'gs-element-blockly.css',                    'text/css'
 end
