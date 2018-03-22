a01d7eb6433cc68f4f13184ffe5cc2ac6fbcde1f
Rodrigo Alfonso <rodri042@gmail.com>
2018-03-22 05:18:30 -0300

    Fixing batch tests

--- spec/precompile_spec.rb
+++ spec/precompile_spec.rb
@@ -129,19 +129,21 @@ examples:
     context 'generates a JSON with the batch request' do
 
       context 'when there is no subject' do
-        let(:expected_code) { "program {\n  PonerDosRojas()\n}\n" + extra.chop }
+        let(:expected_code) { "program {\n  PonerDosRojas()\n}" }
         let(:expected_compilation) {
           [
             {
               initialBoard: "GBB/1.0\nsize 3 3\nhead 0 0\n",
               originalCode: content.chop,
               code: expected_code,
+              extraCode: extra.chop,
               extraBoard: "GBB/1.0\nsize 3 3\nhead 0 1\n"
             },
             {
               initialBoard: "GBB/1.0\nsize 1 1\nhead 0 0\n",
               originalCode: content.chop,
-              code: expected_code
+              code: expected_code,
+              extraCode: extra.chop
             }
           ].to_json
         }
@@ -156,7 +158,8 @@ examples:
             {
               initialBoard: "GBB/1.0\nsize 3 3\nhead 0 0\n",
               originalCode: content,
-              code: expected_code
+              code: expected_code,
+              extraCode: extra.chop
             }
           ].to_json
         }
